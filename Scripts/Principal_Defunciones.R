5175b8f4e218978b875a046355d0aa0f899b1e88
Katiuska Marin <kmmarin@espol.edu.ec>
2020-08-13 02:48:19 -0500

    Analisis de la variable edad

--- Scripts/Principal_Defunciones.R
+++ Scripts/Principal_Defunciones.R
@@ -30,49 +30,99 @@ head(data,5)
 ###3. Análisis comparativo de grupos de edades de fallecimiento por sexo 
 ###  y por cantón de fallecimiento a excepción de Guayaquil.
 
-#Data solo de Guayaquil
-NewData <- subset(data, data$cant_fall == 'Guayaquil')
-NewData
-CantonGqul <- subset(NewData, cant_fall == 'Guayaquil' , "cant_fall")
-colnames(CantonGqul)
-##Grupo de edad 1 
-grupo1TV <- data[data$edad < 10,] ## menores de 10 años, TV:todas las variables
-grupo1Sex <- subset(data, edad < 10, "sexo") 
-subset(data, edad < 10, "edad") 
-grupo1Edad <- subset(data, edad < 10, "edad")
-colnames(grupo1Edad)
+#Data solo de Guayaquil (subset(data, edad == 999 , "edad") => para 1 variable edad)
+DataGquil <- subset(data, data$cant_fall == 'Guayaquil')
+DataGquil
+colnames(DataGquil)
 
-#Frecuencias
-table(data$sexo) #general total hombres y mujeres
-table(data$sexo, data$edad < 10) ##Total de Hombres y mujeres muertos menores de 10 años
-table(data$cant_fall, data$edad < 10)##Total de individuos muertos menores de 10 años x canton5
-table(data$cant_fall, data$sexo)
-table(grupo1TV$cant_fall, grupo1TV$sexo)
+#Data sin informacion de edad
+DataSinEdad <- subset(data, data$edad == 999)
 
-#Fallecimiento por Canton
+#En general Sin Guayaquil y datos sin informacion en edad
+Grupo <- data %>%#Sin Guayaquil
+  filter(! cant_fall %in% DataGquil$cant_fall) %>% 
+  group_by(cant_fall)
 
-data %>%#Sin Guayaquil
-  filter(!cant_fall %in% NewData$cant_fall,
+Grupo <- Grupo %>%#Sin edad
+  filter(! edad %in% DataSinEdad$edad) %>% 
+  group_by(cant_fall)
+
+colnames(Grupo)
+
+#Tablas
+TabEdad3 = table(Grupo$edad)
+TabEdad3
+TablaGeneral3 = table(Grupo$edad, Grupo$sexo) #Tabla x:edad y:cantidad de muertos
+TablaGeneral3
+
+#Diagrama de barras
+  #Freq de defunciones por edad
+barplot(TabEdad3, main="Freq. defunciones por edad" , xlab="Edad")
+  #Histogram
+hist(Grupo$edad, breaks = "Sturges", main = "Histogram de defunciones por Edad",
+     xlab = "Edad") #Usamos la regla de Sturges para las clases
+tablaFreq=table.freq(hist(Grupo$edad, breaks = "Sturges"))
+tablaFreq
+  #Frecuencias relativas
+tablaReltv3 <- fdt(Grupo$edad, breaks = "Sturges")
+tablaReltv3
+  #Densidad
+hist(Grupo$edad, breaks = "Sturges",  main = "Histogram de defunciones por Edad", freq = FALSE)
+lines(density(Grupo$edad), col="blue", lwd=3) # dibujamos la distribuci?n normal emp?rica con los datos que tenemos
+lines(density(Grupo$edad, adjust=2), col="red", lwd=3, lty=2) #suavizar la curva con adjust entre 1 a 5
+abline(v=mean(Grupo$edad), lwd=2, lty=3, col="darkblue") 
+
+plot(tablaR1, type="cfp") #poligono de frecuencias acumulado
+plot(tablaR1, type="rfh") #histograma de frecuencias relativa
+plot(tablaR1, type="fh")
+
+
+
+##Grupo de edad 1: Hombres y mujeres fallecidoss menores de 10 años
+
+#Fallecimiento por Canton sin Guayaquil
+
+Grupo1 <- data %>%#Sin Guayaquil
+  filter(! cant_fall %in% DataGquil$cant_fall,
          between(edad,0,10)) %>% 
-  group_by(cant_fall) %>% 
-  summarise(Total = n())
+  group_by(cant_fall) 
+
+#Tabla de frecuencias para las variables sexo y Canton de fallecimiento
+tablaSexo=table(Grupo1$sexo)
+tablaSexo
+tablaSexFrq=prop.table(tablaSexo) #frecuencias relativas de defunciones por sexo
+tablaSexFrq
+tablaFemen=table(Grupo1$sexo, exclude = c("Hombre")) #tabla solo Masculino
+tablaFemen
+tablaMasc=table(Grupo1$sexo, exclude = c("Mujer")) #tabla solo Masculino
+tablaMasc
+tablaCanton=table(Grupo1$cant_fall)
+tablaCanton
+tablaSexoCanton = table(Grupo1$cant_fall, Grupo1$sexo)
+tablaSexoCanton
+
+
+#Diagrama de barras
+barplot(tablaSexo, main="Diagrama de barras defunciones por sexo" , xlab="Sexo")
 
 data %>%#Con Guayaquil
   group_by(cant_fall) %>% 
   filter(between(edad,0,10)) %>% 
   summarise(Total = n())
 
-#Fallecimiento por sexo según Canton
-NewData %>%
-  group_by(cant_fall, sexo) %>% 
+#Fallecimiento por Canton
+Grupo1 %>%
+  group_by(cant_fall) %>% 
   filter(between(edad,0,10)) %>% 
   summarise(Total = n())
 
 ##Fallecimiento por sexo
-data %>%
+Grupo1 %>%
   group_by(sexo) %>% 
   filter(between(edad,0,10)) %>% 
   summarise(Total = n())
+
+
 
 
 #Masculino Cantones
